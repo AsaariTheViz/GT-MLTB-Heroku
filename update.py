bd94f4b530025fc10a3f1a2febd9dccb11c35125
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2023-10-04 01:11:27 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/Niaz264/Anasty-heroku"
+    UPSTREAM_REPO = "https://github.com/GitXBot101/Happy-MLTB-heroku"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
